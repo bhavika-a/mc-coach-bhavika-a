85ea13c970c4d0dd577e36bf842334e296151ebd
bhavika-a <61728550+bhavika-a@users.noreply.github.com>
2020-03-03 02:11:54 -0800

    Create bhavika bio.rkt

---
+++ bhavika bio.rkt
@@ -0,0 +1,6 @@
+#lang mc-coach/bio
+
+(bio
+ #:first-name "Bhavika"
+ #:profile-pic (square 40 'solid 'red)
+ #:coach-story "Hello! I started coding very recently; just when I started as a coach a few months ago. Before that, I loved learning languages because expanding the languages we speak actually expands the way we think! I have worked with elementary-school children in some capacity for nearly a decade, and I love learning computer languages alongside my students.")
